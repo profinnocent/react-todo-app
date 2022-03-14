361148a1a47a61c7fa2ff41498b061c1a7196ef4
Innocent Unachukwu <innocent.unachukwu@gmail.com>
2022-03-14 23:18:00 +0100

    Worked on Todolist component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
-import './App.css';
-import Header from './Components/Header';
-import Todo from './Components/Todo';
+import "./App.css";
+import Header from "./Components/Header";
+import Todo from "./Components/Todo";
+import React, { useState } from "react";
+import Todolist from "./Components/Todolist";
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [data, setData] = useState([0, 0]);
+  const [inputtext, setInputText] = useState("");
+
+  //console.log(todos);
+
+  //Add New task function
+  const addTask = () => {
+    setTodos([{text: inputtext,time: new Date().getTime() }, ...todos])
+    setInputText("")
+  }
+
   return (
     <div className="App">
-      <Header />
-      <Todo />
+      <Header dataH={data} todosH={todos} />
+      <Todo
+        inputT={inputtext}
+        changeText={(e) => setInputText(e.target.value)}
+        todosT={todos}
+        addTask={addTask}
+      />
+      <Todolist todos={todos}/>
     </div>
   );
 }
--- src/Components/Todo.js
+++ src/Components/Todo.js
@@ -1,189 +1,26 @@
-import React from "react";
 import "./Todo.css";
 
-const Todo = () => {
+const Todo = ({ inputT, changeText, todosT, addTask }) => {
+  // const handleclick = () => {};
+
   return (
     <div className="todo">
       <div className="inputs">
         <h3>Tasks Manager</h3>
         <div className="inputbtn">
-          <input type="text" placeholder="Enter a task here"/>
-          <button>
+          <input
+            type="text"
+            placeholder="Enter a task here"
+            onChange={(e) => changeText(e)}
+            value={inputT}
+          />
+          <button onClick={addTask}>
             Add Task <i className="fa fa-circle-plus"></i>
           </button>
         </div>
       </div>
 
-      <div className="todos">
-        <h3>Todo List</h3>
-        <ul>
-          <li>
-            <div className="li-img">
-              <i className="fa fa-clipboard-list"></i>
-            </div>
-            <div className="content">
-              <h4>Go and watch movie at shoprite and go to a busary the rhg dbfer hjgh ityh.</h4>
-              <span>2-2-22 : 08:15am </span>
-              <span> 3-2-22 10:30pm</span>
-            </div>
-            <div className="btns">
-              <button>Delete</button>
-              <button> -Edit- </button>
-            </div>
-            <input type="checkbox" name="status" id="status" />
-          </li>
-          <li>
-            <div className="li-img">
-              <i className="fa fa-clipboard-list"></i>
-            </div>
-            <div className="content">
-              <h4>Go and watch movie at shoprite</h4>
-              <span>2-2-22 : 08:15am </span>
-              <span> 3-2-22 10:30pm</span>
-            </div>
-            <div className="btns">
-              <button>Delete</button>
-              <button>Edit</button>
-              <input type="checkbox" name="status" id="status" />
-            </div>
-          </li>
-          <li>
-            <div className="li-img">
-              <i className="fa fa-clipboard-list"></i>
-            </div>
-            <div className="content">
-              <h4>Go and watch movie at shoprite</h4>
-              <span>2-2-22 : 08:15am </span>
-              <span> 3-2-22 10:30pm</span>
-            </div>
-            <div className="btns">
-              <button>Delete</button>
-              <button>Edit</button>
-              <input type="checkbox" name="status" id="status" />
-            </div>
-          </li>
-          <li>
-            <div className="li-img">
-              <i className="fa fa-clipboard-list"></i>
-            </div>
-            <div className="content">
-              <h4>Go and watch movie at shoprite</h4>
-              <span>2-2-22 : 08:15am </span>
-              <span> 3-2-22 10:30pm</span>
-            </div>
-            <div className="btns">
-              <button>Delete</button>
-              <button>Edit</button>
-              <input type="checkbox" name="status" id="status" />
-            </div>
-          </li>
-          <li>
-            <div className="li-img">
-              <i className="fa fa-clipboard-list"></i>
-            </div>
-            <div className="content">
-              <h4>Go and watch movie at shoprite</h4>
-              <span>2-2-22 : 08:15am </span>
-              <span> 3-2-22 10:30pm</span>
-            </div>
-            <div className="btns">
-              <button>Delete</button>
-              <button>Edit</button>
-              <input type="checkbox" name="status" id="status" />
-            </div>
-          </li>
-          <li>
-            <div className="li-img">
-              <i className="fa fa-clipboard-list"></i>
-            </div>
-            <div className="content">
-              <h4>Go and watch movie at shoprite</h4>
-              <span>2-2-22 : 08:15am </span>
-              <span> 3-2-22 10:30pm</span>
-            </div>
-            <div className="btns">
-              <button>Delete</button>
-              <button>Edit</button>
-              <input type="checkbox" name="status" id="status" />
-            </div>
-          </li>
-          <li>
-            <div className="li-img">
-              <i className="fa fa-clipboard-list"></i>
-            </div>
-            <div className="content">
-              <h4>Go and watch movie at shoprite</h4>
-              <span>2-2-22 : 08:15am </span>
-              <span> 3-2-22 10:30pm</span>
-            </div>
-            <div className="btns">
-              <button>Delete</button>
-              <button>Edit</button>
-              <input type="checkbox" name="status" id="status" />
-            </div>
-          </li>
-          <li>
-            <div className="li-img">
-              <i className="fa fa-clipboard-list"></i>
-            </div>
-            <div className="content">
-              <h4>Go and watch movie at shoprite</h4>
-              <span>2-2-22 : 08:15am </span>
-              <span> 3-2-22 10:30pm</span>
-            </div>
-            <div className="btns">
-              <button>Delete</button>
-              <button>Edit</button>
-              <input type="checkbox" name="status" id="status" />
-            </div>
-          </li>
-          <li>
-            <div className="li-img">
-              <i className="fa fa-clipboard-list"></i>
-            </div>
-            <div className="content">
-              <h4>Go and watch movie at shoprite</h4>
-              <span>2-2-22 : 08:15am </span>
-              <span> 3-2-22 10:30pm</span>
-            </div>
-            <div className="btns">
-              <button>Delete</button>
-              <button>Edit</button>
-              <input type="checkbox" name="status" id="status" />
-            </div>
-          </li>
-          <li>
-            <div className="li-img">
-              <i className="fa fa-clipboard-list"></i>
-            </div>
-            <div className="content">
-              <h4>Go and watch movie at shoprite</h4>
-              <span>2-2-22 : 08:15am </span>
-              <span> 3-2-22 10:30pm</span>
-            </div>
-            <div className="btns">
-              <button>Delete</button>
-              <button>Edit</button>
-              <input type="checkbox" name="status" id="status" />
-            </div>
-          </li>
-          <li>
-            <div className="li-img">
-              <i className="fa fa-clipboard-list"></i>
-            </div>
-            <div className="content">
-              <h4>Go and watch movie at shoprite</h4>
-              <span>2-2-22 : 08:15am </span>
-              <span> 3-2-22 10:30pm</span>
-            </div>
-            <div className="btns">
-              <button>Delete</button>
-              <button>Edit</button>
-              <input type="checkbox" name="status" id="status" />
-            </div>
-          </li>
-        </ul>
-      </div>
+      
     </div>
   );
 };
